bb9aaf57b62caac540e3d3ff87b951578715bb50
Vinícius Bonetti Franceschi <vinibfranc@gmail.com>
2017-04-03 16:16:36 -0300

    Update Calculadora.java

--- src/modelo/Calculadora.java
+++ src/modelo/Calculadora.java
@@ -1,46 +1,7 @@
 package modelo;
-// classe que contém as regras de negócio da aplicação
+// classe que contÃ©m as regras de negÃ³cio da aplicaÃ§Ã£o
 public class Calculadora {
-	// ----> Não é necessária a inclusão de atributos ou métodos específicos para esta classe, apenas o método estático
-
-	/*
-	private int num1;
-	private int num2;
-	private int resultado;
-	
-	// construtor da calculadora
-	public Calculadora(int num1, int num2, int resultado) {
-		this.num1 = num1;
-		this.num2 = num2;
-		this.resultado = resultado;
-	}
-	// getters e setters
-	public int getNum1() {
-		return num1;
-	}
-
-	public void setNum1(int num1) {
-		this.num1 = num1;
-	}
-
-	public int getNum2() {
-		return num2;
-	}
-
-	public void setNum2(int num2) {
-		this.num2 = num2;
-	}
-
-	public int getResultado() {
-		return resultado;
-	}
-
-	public void setResultado(int resultado) {
-		this.resultado = resultado;
-	}
-	*/
-
-	// método que calcula a soma entre os dois números informados
+	// mÃ©todo que calcula a soma entre os dois nÃºmeros informados
 	public static int somaDoisNumeros(int x, int y) {
 		return x + y;
 	}
