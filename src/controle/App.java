2a0b05b6df929ebcb0fec6952eddc24bc8c14fa7
Vinícius Bonetti Franceschi <vinibfranc@gmail.com>
2017-04-03 16:18:00 -0300

    Update App.java

--- src/controle/App.java
+++ src/controle/App.java
@@ -4,20 +4,13 @@ import java.awt.EventQueue;
 import visao.JanelaPrincipal;
 
 public class App {
-	// método principal da aplicação
+	// mÃ©todo principal da aplicaÃ§Ã£o
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					JanelaPrincipal janela = new JanelaPrincipal();
 					Controlador controle = new Controlador(janela);
-					
-					// ----> As seguintes funções podem ser postas no construtor de JanelPrincipal
-
-					/*janela.setVisible(true);
-					janela.getTxtPrimeiroNum().grabFocus(); // ajusta o foco para o textField do primeiro número
-					janela.setResizable(false);
-					janela.setLocationRelativeTo(null); // centralização na janela no centro da tela*/
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
